7e984d6613892f52115510ddb8e14014988e7faf
Rishaan Sharma <rishaan.sharma2202@gmail.com>
2026-09-05 18:52:21 +0530

    Fix positioning of text in /review

--- app/(loggedIn)/review/page.js
+++ app/(loggedIn)/review/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>
+    <div classname="absolute left-50 top-20">
       <h1>Review</h1>
       <p>Welcome to the review page!</p>
     </div>
