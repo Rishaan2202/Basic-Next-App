0a623eee18cc5f685775aa56f85c2137a454232b
Rishaan Sharma <rishaan.sharma2202@gmail.com>
2026-09-09 18:37:05 +0530

    Remove merge conflict markers from explore page

--- app/(loggedIn)/explore/page.js
+++ app/(loggedIn)/explore/page.js
@@ -1,17 +1,11 @@
 import { getDatabase } from "@/lib/mongodb";
 
 export default async function ExplorePage() {
-<<<<<<< HEAD
 
   const db = await getDatabase();
   const projects = await db.collection("userData").find({}, { projection: { "_id": 0, "event_details.projects": 1 } }).toArray();
   const projectsList = projects.flatMap(user => user.event_details?.projects || []);
-
-=======
-  const db = await getDatabase();
-  const projects = await db.collection("userData").find({}, { projection: { "_id": 0, "event_details.projects": 1 } }).toArray();
-  const projectsList = projects.flatMap(user => user.event_details?.projects || []);
->>>>>>> ffddf3c6d9a20f962c9c68db7cdc8cb225ad57ae
+  
   console.log("Explore data fetched from MongoDB:", projectsList);
 
   return (
