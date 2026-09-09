5d00ce5bef9166903cac83ab983cc91a7f811495
Rishaan Sharma <rishaan.sharma2202@gmail.com>
2026-09-09 18:28:25 +0530

    Remove merge conflict marker from not-found.js
    Removed merge conflict markers and simplified heading.

--- app/not-found.js
+++ app/not-found.js
@@ -3,11 +3,7 @@ import React from 'react'
 const NotFound = () => {
   return (
     <div>
-<<<<<<< HEAD
       <h1 className='text-10xl'>Page Not Found</h1>
-=======
-      <h1>Page Not Found</h1>
->>>>>>> ffddf3c6d9a20f962c9c68db7cdc8cb225ad57ae
       <p>The page you are looking for does not exist.</p>
     </div>
   )
