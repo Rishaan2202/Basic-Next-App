7343b12069be78e644e7bef79922ce56cc360f33
Rishaan Sharma <rishaan.sharma2202@gmail.com>
2026-09-09 18:34:27 +0530

    Remove merge conflict marker from /explore/page.js
    Refactor project list rendering to use a grid layout.

--- app/(loggedIn)/explore/page.js
+++ app/(loggedIn)/explore/page.js
@@ -18,7 +18,6 @@ export default async function ExplorePage() {
     <div className="absolute left-50 top-20">
       <h1 className='font-bold text-3xl'>Explore</h1>
       <p>Welcome to the explore page!</p>
-<<<<<<< HEAD
       <ul className="grid grid-cols-2 h-full">
         {projectsList.map((user, index) => (
           <li key={index} className="flex flex-col break-words bg-[var(--tertiary)] m-2 p-2 rounded text-black w-[30vw]">
@@ -27,16 +26,6 @@ export default async function ExplorePage() {
             <div className='flex justify-around mt-auto'>
               <button className="bg-[var(--secondary)] hover:bg-[var(--secondary)] hover:cursor-pointer hover:scale-[1.1] m-2 text-black font-bold py-2 px-4 rounded w-[45%]"><a href={user.demo || "#"} target="_blank" rel="noopener noreferrer">Demo</a></button>
               <button className="bg-[var(--secondary)] hover:bg-[var(--secondary)] hover:cursor-pointer hover:scale-[1.1] m-2 text-black font-bold py-2 px-4 rounded w-[45%] "><a href={user.code || "#"} target="_blank" rel="noopener noreferrer">Code</a></button> 
-=======
-      <ul>
-        {projectsList.map((user, index) => (
-          <li key={index} className="bg-[var(--tertiary)] m-2 p-2 rounded text-black w-[30vw]">
-            <h2 className="font-bold text-2xl flex justify-center">{user.name || "Project Name"}</h2> 
-            <p>{user.description || "Project Description"}</p>
-            <div className='flex justify-around'>
-              <button className="bg-[var(--secondary)] hover:bg-[var(--secondary)] hover:scale-[1.1] m-2 text-black font-bold py-2 px-4 rounded w-[45%]"><a href={user.demo || "#"} target="_blank" rel="noopener noreferrer">Demo</a></button>
-              <button className="bg-[var(--secondary)] hover:bg-[var(--secondary)] hover:scale-[1.1] m-2 text-black font-bold py-2 px-4 rounded w-[45%]"><a href={user.code || "#"} target="_blank" rel="noopener noreferrer">Code</a></button> 
->>>>>>> ffddf3c6d9a20f962c9c68db7cdc8cb225ad57ae
             </div>
           </li>
         ))}
